926fce24f3db740a3931079a1fe1193280b09296
Carro <carolinaisabellaandersson@gmail.com>
2023-09-05 17:09:58 +0200

    login function server side

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const cookieSession = require("cookie-session");
 
 const { userRouter } = require("./routes/user.router");
 const app = express();
@@ -9,6 +10,17 @@ app.use(express.json());
 app.use(
   cors({
     origin: "*",
+  })
+);
+
+app.use(
+  cookieSession({
+    name: "session",
+    keys: ["aVeryS3cr3tK3y"],
+    maxAge: 1000 * 60 * 60 * 24, // 24 Hours
+    sameSite: "strict",
+    httpOnly: true,
+    secure: false,
   })
 );
 

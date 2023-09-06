38a850d7be4ba0b636c84358b3c47cf0547cfbf4
Carro <carolinaisabellaandersson@gmail.com>
2023-09-06 20:42:25 +0200

    Client setup and login function

---
+++ client/src/components/RegisterUser.tsx
@@ -0,0 +1,23 @@
+function RegisterUser() {
+  return (
+    <div>
+      <h2>Register User</h2>
+      <form>
+        <label>
+          Enter your name:
+          <input type="text" />
+        </label>
+        <label>
+          Enter your email:
+          <input type="text" />
+        </label>
+        <label>
+          Enter your password:
+          <input type="text" />
+        </label>
+      </form>
+    </div>
+  );
+}
+
+export default RegisterUser;
